f2de1be8f7558286ccf32803de07f5cac156193e
egnkvn <74886954+egnkvn@users.noreply.github.com>
2022-01-18 00:23:19 +0800

    Update axios.js

--- final/src/axios.js
+++ final/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ROOT = "https://moosetherecord3.herokuapp.com/:5000"
+const API_ROOT = "https://moosetherecord3.herokuapp.com/"
 
 const instance = axios.create({
     baseURL: API_ROOT,
