43518d49fa906e7afb968253546b7464a064dfef
f776655321 <f77665532166@gmail.com>
2022-01-18 03:26:31 +0800

    hi

--- final/server.js
+++ final/server.js
@@ -14,13 +14,22 @@ import { fileURLToPath } from "url";
   // define routes
   app.use('/api',Router)
   // define server
-  app.use(express.static(path.join(__dirname, "build")));
-  app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
-  app.get("/calendar", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+    app.get("/calendar", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+  }
+  // app.use(express.static(path.join(__dirname, "build")));
+  // app.get("/", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "build", "index.html"));
+  // });
+  // app.get("/calendar", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "build", "index.html"));
+  // });
   const port = process.env.PORT || 5000
   app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
